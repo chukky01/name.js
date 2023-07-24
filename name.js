943ea1f57cfcacb0922109463081dfdd3664ce12
chukky01 <85424159+chukky01@users.noreply.github.com>
2023-07-24 11:33:56 +0100

    The full name.js program

---
+++ name.js
@@ -0,0 +1,10 @@
+//To run this program, you can save it as a .js file and then open it in a web browser.
+
+const prompt = require('prompt-sync')();
+
+//Create variables for the firs and last names
+var fname = prompt("Enter your first name: ");
+var lname = prompt("Enter your last name: ");
+
+//Add the two names together and then output the result
+console.log("Your name is, ", fname, lname);
